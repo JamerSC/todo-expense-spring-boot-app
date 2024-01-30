6e8e0111ff1286054c2b6f5f8d166df0b73da04d
Jamer Catalla <mr.catalla28@gmail.com>
2024-01-30 23:45:02 +0800

    Second commit, add thymeleaf

--- src/main/java/com/jamersc/springboot/todoexpense/rest/RestController.java
+++ src/main/java/com/jamersc/springboot/todoexpense/rest/RestController.java
@@ -1,13 +1,18 @@
 package com.jamersc.springboot.todoexpense.rest;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@org.springframework.web.bind.annotation.RestController
+@Controller
 public class RestController {
 
     @GetMapping("/")
-    public String helloWorld() {
-        return "Hello world!";
+    public String showIndex(Model theModel) {
+
+        theModel.addAttribute("theDate", new java.util.Date());
+
+        return "index";
     }
 }
