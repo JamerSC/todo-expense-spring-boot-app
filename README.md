cb0ac8073e957170fd778c370d5d9c1a8fabfd41
Jamer Catalla <mr.catalla28@gmail.com>
2024-02-07 23:24:15 +0800

    Fourth commit prepare front-end design

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A todo list & expense tracker app.
+January create a spring initializer & setup.
+February Create a Classes & Setup front end.
--- src/main/java/com/jamersc/springboot/todoexpense/rest/RestController.java
+++ src/main/java/com/jamersc/springboot/todoexpense/rest/RestController.java
@@ -4,14 +4,17 @@ package com.jamersc.springboot.todoexpense.rest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/todoexpense")
 public class RestController {
 
-    @GetMapping("/")
-    public String showIndex(Model theModel) {
+    @GetMapping("/index")
+    public String showIndex() {
+    // public String showIndex(Model theModel) {
 
-        theModel.addAttribute("theDate", new java.util.Date());
+        // theModel.addAttribute("theDate", new java.util.Date());
 
         return "index";
     }
