24fbe05a96a88a3e5a6a0460de947041727d1811
Jamer Catalla <mr.catalla28@gmail.com>
2024-03-22 22:14:32 +0800

    13th commit - added JpaRepository & get mapping, fetching list of Users to DB.

--- src/main/java/com/jamersc/springboot/todoexpense/controller/RestController.java
+++ src/main/java/com/jamersc/springboot/todoexpense/controller/RestController.java
@@ -1,14 +1,34 @@
 package com.jamersc.springboot.todoexpense.controller;
 
 
+import com.jamersc.springboot.todoexpense.dao.UserRepository;
+import com.jamersc.springboot.todoexpense.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import java.util.List;
+
+@org.springframework.web.bind.annotation.RestController
+//@Controller
 @RequestMapping("/todoexpense")
 public class RestController {
+
+    // Using JpaRepository for Accessing DB & test API
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // testing REST API in postman
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello! Start recording your work now!";
+    }
 
     @GetMapping("/showLoginSignup")
     public String showLoginSignup() {
