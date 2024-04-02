712e4d182df16e46dff3865fdb10a1d0d1a53be1
Jamer Catalla <mr.catalla28@gmail.com>
2024-04-02 09:32:25 +0800

    21c - Test thymeleaf model -.login & processLogin methods.

--- src/main/java/com/jamersc/springboot/todoexpense/controller/TodoexpenseController.java
+++ src/main/java/com/jamersc/springboot/todoexpense/controller/TodoexpenseController.java
@@ -3,15 +3,33 @@ package com.jamersc.springboot.todoexpense.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TodoexpenseController {
+
+
+    // Show form
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // Process form
+    @RequestMapping("/processLogin")
+    public String processLogin() {
+        return "index";
+    }
+
+    /*
 
     @GetMapping("/login")
     public String showLogin(Model theModel) {
 
         theModel.addAttribute("theDate", java.time.LocalDateTime.now());
 
-        return "login-signup";
+        return "login";
     }
+
+    */
 }
