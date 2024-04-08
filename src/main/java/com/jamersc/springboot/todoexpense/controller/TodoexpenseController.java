2f8df03180c7c06dc544bb6b3463c3a2d9fe2dc1
Jamer Catalla <mr.catalla28@gmail.com>
2024-04-08 22:35:58 +0800

    24c - Update controller, login, & index page. test form data, model & attribute, & data binding.

--- src/main/java/com/jamersc/springboot/todoexpense/controller/TodoexpenseController.java
+++ src/main/java/com/jamersc/springboot/todoexpense/controller/TodoexpenseController.java
@@ -1,50 +1,50 @@
 package com.jamersc.springboot.todoexpense.controller;
 
+import com.jamersc.springboot.todoexpense.entity.User;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TodoexpenseController {
 
+    // Show form & Create a model
     @GetMapping("/showLogin")
-    public String showLogin() {
+    public String showLogin(Model model) {
+        // create a user object
+        User user = new User();
+        // create a model
+        model.addAttribute("user", user);
+        // redirect to login page
         return "login";
     }
 
-    // Show form
-    @RequestMapping("/login")
-    public String login() {
-        return "login";
-    }
+    @PostMapping("/processLoginForm")
+    public String processLoginForm(@ModelAttribute("user") User theUser){
 
-    // Process form
-    @RequestMapping("/processLogin")
-    public String processLogin() {
+        //log input data
+        System.out.println("theUser username: " + theUser.getUsername());
+
         return "index";
     }
+
+
+
 
 
     // Http Servlet Process
     @RequestMapping("/processLoginSecond")
     public String processLoginSecond(HttpServletRequest request, Model model) {
-
         // Create an object & get the parameter
         String theUsername = request.getParameter("username");
-
         // Convert the object into upper case
         theUsername = theUsername.toUpperCase();
-
         // create a message
         String result = "Hey " + theUsername;
-
         // add message to the model
         model.addAttribute("message", result);
-
         return "index";
     }
 
@@ -52,30 +52,26 @@ public class TodoexpenseController {
     @PostMapping("/processLoginThird")
     public String processLoginThird(
             @RequestParam("username") String theUsername, Model model) {
-
         // Convert the object into upper case
         theUsername = theUsername.toUpperCase();
-
         // create a message
         String result = "Hey " + theUsername;
-
         // add message to the model
         model.addAttribute("message", result);
-
         return "index";
     }
 
-    @RequestMapping("/todoExpense/showIndex")
+    @GetMapping("/showIndex")
     public String showIndex() {
         return "index";
     }
 
-    @RequestMapping("/todoExpense/showTodo")
+    @GetMapping("showTodo")
     public String showTodo() {
         return "todo";
     }
 
-    @RequestMapping("/todoExpense/showExpenses")
+    @GetMapping("/showExpenses")
     public String showExpenses() {
         return "expenses";
     }
