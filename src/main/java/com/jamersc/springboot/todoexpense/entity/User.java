6707cba9fd72f13ec857fe04d84aca3aacfd42c7
Jamer Catalla <mr.catalla28@gmail.com>
2024-04-18 10:08:13 +0800

    32c2 - Changes: User entity & Added @InitBinder on User ctrlr, test validation.

--- src/main/java/com/jamersc/springboot/todoexpense/entity/User.java
+++ src/main/java/com/jamersc/springboot/todoexpense/entity/User.java
@@ -1,8 +1,6 @@
 package com.jamersc.springboot.todoexpense.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -17,13 +15,15 @@ public class User {
     @Column(name = "user_id")
     private int userId;
 
-//    @NotBlank(message = "First name is required!")
-//    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+//    @NotNull(message = "First name is required!")
+//    @Size(min = 1, message = "First name is required!")
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
 //    @NotBlank(message = "Last name is required!")
 //    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+//    @NotNull(message = "First name is required!")
+//    @Size(min = 1, message = "First name is required!")
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
@@ -32,15 +32,17 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    @NotBlank(message = "Username is required!")
-    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+    @NotNull(message = "Username is required!")
+    @Size(min = 1, message = "Username is required!")
+ // @Size(min = 1, max = 20, message = "Username must be between 4 and 20 characters")
     @Column(name = "username", nullable = false, unique = true)
-    private String username;
+    private String username = "";
 
-    @NotBlank(message = "Password is required!")
-    @Size(min = 6, message = "Password must be at least 6 characters")
+    @NotNull(message = "Password is required!")
+    @Size(min = 1, message = "Password is required!")
+ //   @Size(min = 6, message = "Password must be at least 6 characters")
     @Column(name = "password", nullable = false)
-    private String password;
+    private String password = "";
 
 //  @NotBlank(message = "Email is required!")
 //  @Email(message = "Invalid email format")
